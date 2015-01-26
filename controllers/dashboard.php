d38f6791985cc301ba7dfa6e63187825a2f5f8bb
ScoL502 <Anton@iMac-Anton.local>
2015-01-26 20:35:00 +0300

    add templater
    some templater

--- controllers/dashboard.php
+++ controllers/dashboard.php
@@ -1,9 +1,13 @@
 <?php
-  class index extends Controller {
+  class dashboard extends Controller {
    	public function __construct() {
     	parent::__construct();
     	$this->view->load('dashboard/index.php');
-    	$this->view->render();
+  	}
+
+  	public function index() {
+  		$this->view->set('[_name]','Валовая выручка:');
+  		$this->view->render();
   	}
   }
 ?>
--- libs/view.php
+++ libs/view.php
@@ -1,5 +1,8 @@
 <?php
   class View {
+
+  public $model_view;
+
    public function __construct() {
    	$this->main_view = file(ROOT_DIR.'/views/index.php');
    	$this->main_view = implode('',$this->main_view);
@@ -8,7 +11,11 @@
    	$this->model_view = file(ROOT_DIR.'/views/'.$name);
    	$this->model_view = implode('',$this->model_view);
    }
+   public function set($name, $value) {
+    $this->model_view = str_replace($name,$value,$this->model_view);
+   }
    public function render() {
+    $this->main_view = str_replace('[_content]', $this->model_view, $this->main_view);
     echo $this->main_view;
    }
   }
